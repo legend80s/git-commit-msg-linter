1f389bda675dfec4abff89032fedc305febf54d8
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2019-05-13 23:18:40 +0800

    feat: enable "commitlinterrc.json"

--- commit-msg.js
+++ commit-msg.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 // Any line of the commit message cannot be longer 100 characters!
 // This allows the message to be easier to read on github as well as in various git tools.
@@ -14,7 +18,7 @@ const GRAY = '\x1B[0;37m';
 const RED = '\x1B[0;31m';
 const GREEN = '\x1B[0;32m';
 
-const TYPES = {
+const STEREOTYPES = {
   feat: 'New feature.',
   fix: 'Bug fix.',
   docs: 'Documentation.',
@@ -27,16 +31,134 @@ const TYPES = {
   temp: 'Temporary commit that won\'t be included in your CHANGELOG.',
 };
 
-const types = Object.keys(TYPES);
-const maxTypeLength = [...types].sort((t1, t2) => t2.length - t1.length)[0].length;
+const commitMsg = process.argv[2];
+const commitlinterrc = path.resolve(__dirname, '..', '..', 'commitlinterrc.json');
 
-const typeDescriptions = types
-  .map((type, index) => {
-    const description = TYPES[type];
+// console.log(commitlinterrc);
 
-    return generateTypeDescription({ index, type, description, maxTypeLength });
-  })
-  .join('\n');
+main(commitMsg, commitlinterrc);
+
+/**
+ * main function
+ *
+ * @param {string} commitMsgFile
+ * @param {string} commitlinterrcFile
+ *
+ * @returns {void}
+ */
+async function main(commitMsgFile, commitlinterrcFile) {
+  const [commitMsgContent, config] = await Promise.all([
+    readFile(commitMsgFile),
+    readConfig(commitlinterrcFile),
+  ]);
+
+  const merged = Object.assign({}, STEREOTYPES, config.types);
+  const msg = getFirstLine(commitMsgContent);
+
+  if (!validateMessage(msg, merged)) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+}
+
+/**
+ * @param {string} filename
+ * @returns {Promise<Object>} return empty object when file not exist
+ * @throws {Error} when error code of read file is not `ENOENT` or file exist but its content is invalid json
+ */
+async function readConfig(filename) {
+  const packageName = `${YELLOW}git-commit-msg-linter`;
+  let content = '{}';
+
+  try {
+    content = await readFile(filename);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      /** pass, as commitlinterrc are optional */
+    } else {
+      /** pass, commitlinterrc ignored when invalid */
+      /** It must be a bug so output the error to the user */
+      console.error(`${packageName}: ${RED}read commitlinterrc.json failed`, error);
+    }
+  }
+
+  let config = {};
+
+  try {
+    config = JSON.parse(content)
+  } catch (error) {
+    /** pass, commitlinterrc ignored when invalid json */
+    /** output the error to the user for self-checking */
+    console.error(`${packageName}: ${RED}commitlinterrc.json ignored because of invalid json`);
+  }
+
+  return config;
+}
+
+function getFirstLine(buffer) {
+  return buffer.toString().split('\n').shift();
+}
+
+/**
+ * validate git message
+ *
+ * @param {string} message
+ * @param {Object>} mergedTypes
+ * @returns {boolean}
+ */
+function validateMessage(message, mergedTypes) {
+  let isValid = true;
+  let invalidLength = false;
+
+  if (IGNORED.test(message)) {
+    console.log('Commit message validation ignored.');
+    return true;
+  }
+
+  if (message.length > MAX_LENGTH) {
+    invalidLength = true;
+    isValid = false;
+  }
+
+  // if (/[\u3220-\uFA29]+/.test(message)) {
+  //   console.error(`${COLOR}commit text can not contain chinese characters`);
+  //   isValid = false;
+  // }
+
+  const match = PATTERN.exec(message);
+
+  if (!match) {
+    displayError({ invalidLength, invalideFormat: true, mergedTypes });
+    return false;
+  }
+
+  const type = match[1];
+  const scope = match[3];
+  const subject = match[4];
+
+  // console.log('type:', type);
+  // console.log('scope:', scope);
+  // console.log('subject:', subject);
+
+  const types = Object.keys(mergedTypes);
+  const invalideType = !types.includes(type);
+
+  // scope can be optional, but not empty string
+  // "test: hello" OK
+  // "test(): hello" FAILED
+  const invalidScope = typeof scope === 'string' && scope.trim() === '';
+
+  // Don't capitalize first letter; No dot (.) at the end
+  const invalidSubject = isUpperCase(subject[0]) || subject.endsWith('.');
+
+  if (invalideType || invalidScope || invalidSubject) {
+    displayError({ invalideType, invalidScope, invalidSubject, mergedTypes });
+    return false;
+  }
+
+  return isValid;
+}
 
 /* eslint-disable no-console */
 function displayError({
@@ -45,10 +167,12 @@ function displayError({
   invalideType = false,
   invalidScope = false,
   invalidSubject = false,
+  mergedTypes,
 } = {}) {
   const type = invalideType ? `${RED}<type>` : '<type>';
   const scope = invalidScope ? `${RED}(<scope>)` : `${invalideFormat ? RED : GREEN}(<scope>)`;
   const subject = invalidSubject ? `${RED}<subject>` : `${invalideFormat ? RED : GREEN}<subject>`;
+  const typeDescriptions = describeTypes(mergedTypes);
 
   console.error(
     `
@@ -78,58 +202,6 @@ function displayError({
   );
 }
 
-function validateMessage(message) {
-  let isValid = true;
-  let invalidLength = false;
-
-  if (IGNORED.test(message)) {
-    console.log('Commit message validation ignored.');
-    return true;
-  }
-
-  if (message.length > MAX_LENGTH) {
-    invalidLength = true;
-    isValid = false;
-  }
-
-  // if (/[\u3220-\uFA29]+/.test(message)) {
-  //   console.error(`${COLOR}commit text can not contain chinese characters`);
-  //   isValid = false;
-  // }
-
-  const match = PATTERN.exec(message);
-
-  if (!match) {
-    displayError({ invalidLength, invalideFormat: true });
-    return false;
-  }
-
-  const type = match[1];
-  const scope = match[3];
-  const subject = match[4];
-
-  // console.log('type:', type);
-  // console.log('scope:', scope);
-  // console.log('subject:', subject);
-
-  const invalideType = !types.includes(type);
-
-  // scope can be optional, but not empty string
-  // "test: hello" OK
-  // "test(): hello" FAILED
-  const invalidScope = typeof scope === 'string' && scope.trim() === '';
-
-  // Don't capitalize first letter; No dot (.) at the end
-  const invalidSubject = isUpperCase(subject[0]) || subject.endsWith('.');
-
-  if (invalideType || invalidScope || invalidSubject) {
-    displayError({ invalideType, invalidScope, invalidSubject });
-    return false;
-  }
-
-  return isValid;
-}
-
 /**
  * isUpperCase
  * @param {string} letter
@@ -138,24 +210,8 @@ function isUpperCase(letter) {
   return /^[A-Z]$/.test(letter);
 }
 
-function firstLineFromBuffer(buffer) {
-  return buffer.toString().split('\n').shift();
-}
-
-const commitMsgFile = process.argv[2];
-
-fs.readFile(commitMsgFile, (err, buffer) => {
-  const msg = firstLineFromBuffer(buffer);
-
-  if (!validateMessage(msg)) {
-    process.exit(1);
-  } else {
-    process.exit(0);
-  }
-});
-
 /**
- * return n spaces
+ * return a string of n spaces
  *
  * @param  {number}
  * @return {string}
@@ -167,18 +223,37 @@ function nSpaces(n) {
 }
 
 /**
- * generate type description
+ * generate a type description
  *
- * @param {number} options.index
- * @param {string} options.type
- * @param {string} options.description
- * @param {number} options.maxTypeLength
+ * @param {number} options.index type index
+ * @param {string} options.type type name
+ * @param {string} options.description type description
+ * @param {number} options.maxLength max type length
  *
  * @returns {string}
  */
-function generateTypeDescription({ index, type, description, maxTypeLength }) {
+function describe({ index, type, description, maxLength }) {
   const paddingBefore = index === 0 ? '' : nSpaces(4);
-  const marginRight = nSpaces(maxTypeLength - type.length + 1);
+  const marginRight = nSpaces(maxLength - type.length + 1);
 
   return `${paddingBefore}${YELLOW}${type}${marginRight}${GRAY}${description}`;
+}
+
+/**
+ * generate type descriptions
+ *
+ * @param {Object} typeConfig
+ * @returns {string} type descriptions
+ */
+function describeTypes(typeConfig) {
+  const types = Object.keys(typeConfig);
+  const maxLength = [...types].sort((t1, t2) => t2.length - t1.length)[0].length;
+
+  return types
+    .map((type, index) => {
+      const description = typeConfig[type];
+
+      return describe({ index, type, description, maxLength });
+    })
+    .join('\n');
 }
