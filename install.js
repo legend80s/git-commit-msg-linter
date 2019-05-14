1a4e5108b5c7e2220b6470ed8e5e7a5293988c43
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2019-05-14 14:49:40 +0800

    style: format code

--- install.js
+++ install.js
@@ -1,7 +1,7 @@
 /**
- * On installing, it will copy the `{PROJECT}/.git/hooks/commit-msg` executable file if exists
+ * When installing, it will copy the executable file `{PROJECT}/.git/hooks/commit-msg` if it exists
  * to `{PROJECT}/.git/hooks/commit-msg.old`
- * then the `commit-msg` will be overwrote by injecting the linting rules.
+ * then the `commit-msg` will be overwritten by our linting rules.
  */
 
 /* eslint-disable no-console */
@@ -9,13 +9,20 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+
 const { bailOut } = require('./utils');
-const { COMMIT_MSG_LABLE, PACKAGE_NAME_LABEL, COMMIT_MSG_HOOK_FILE, PACKAGE_NAME, PROJECT_ROOT } = require('./constants');
+const {
+  COMMIT_MSG_LABLE,
+  PACKAGE_NAME_LABEL,
+  COMMIT_MSG_HOOK_FILE,
+  PACKAGE_NAME,
+  PROJECT_ROOT,
+} = require('./constants');
 
 const exists = fs.existsSync;
 const git = path.resolve(PROJECT_ROOT, '.git');
 
-// Bail out if we don't have an `.git` directory as the hooks will not get triggered.
+// Bail out if we don't have an `.git` folder as the hooks will not get triggered.
 if (!exists(git) || !fs.lstatSync(git).isDirectory()) {
   console.error(`${PACKAGE_NAME_LABEL}: ${chalk.red('.git folder Not found')}`);
   console.error(`${PACKAGE_NAME_LABEL}: ${chalk.red(`${PACKAGE_NAME} won't be installed`)}`);
@@ -26,12 +33,11 @@ if (!exists(git) || !fs.lstatSync(git).isDirectory()) {
 const hooks = path.resolve(git, 'hooks');
 const commitMsgHookFile = path.resolve(hooks, COMMIT_MSG_HOOK_FILE);
 
-// If we do have directory create a hooks folder if it doesn't exist.
+// If we do have `.git` folder create a `hooks` folder under it if it doesn't exist.
 if (!exists(hooks)) { fs.mkdirSync(hooks); }
 
-// If there's an existing `commit-msg` hook we want to back it up instead of
-// overriding it and losing it completely as it might contain something
-// important.
+// If there's an existing `commit-msg` hook file back it up instead of
+// overwriting it and losing it completely as it might contain something important.
 if (exists(commitMsgHookFile) && !fs.lstatSync(commitMsgHookFile).isSymbolicLink()) {
   console.log(`${PACKAGE_NAME_LABEL}:`);
   console.log(`${PACKAGE_NAME_LABEL}: An existing git ${COMMIT_MSG_LABLE} hook detected`);
@@ -46,8 +52,8 @@ if (exists(commitMsgHookFile) && !fs.lstatSync(commitMsgHookFile).isSymbolicLink
 const rules = fs.readFileSync('./commit-msg.js');
 
 // It could be that we do not have rights to this folder which could cause the
-// installation of this module to completely fail. We should just output the
-// error instead destroying the whole npm install process.
+// installation of this module to completely failure.
+// We should just output the error instead breaking the whole npm install process.
 try { fs.writeFileSync(commitMsgHookFile, rules); } catch (e) {
   console.error(`${PACKAGE_NAME_LABEL}: ${chalk.red('Failed to create the hook file in your .git/hooks folder because:')}`);
   console.error(`${PACKAGE_NAME_LABEL}: ${chalk.red(e.message)}`);
--- uninstall.js
+++ uninstall.js
@@ -1,13 +1,20 @@
 /**
- * On uninstalling, the `commit-msg` file will be restored and the `commit-msg.old` will be deleted.
+ * When uninstalling, the `commit-msg` file will be restored
+ * and the `commit-msg.old` will be removed.
  */
 
 /* eslint-disable no-console */
+
 const fs = require('fs');
 const path = require('path');
 
-const { COMMIT_MSG_HOOK_FILE, PACKAGE_NAME_LABEL, COMMIT_MSG_LABLE, PROJECT_ROOT } = require('./constants');
 const { bailOut } = require('./utils');
+const {
+  COMMIT_MSG_HOOK_FILE,
+  PACKAGE_NAME_LABEL,
+  COMMIT_MSG_LABLE,
+  PROJECT_ROOT,
+} = require('./constants');
 
 const exists = fs.existsSync || path.existsSync;
 const git = path.resolve(PROJECT_ROOT, '.git');
@@ -15,17 +22,20 @@ const git = path.resolve(PROJECT_ROOT, '.git');
 // Location of hook file, if it exists
 const commitMsgFile = path.resolve(git, 'hooks', COMMIT_MSG_HOOK_FILE);
 
-// Bail out if we don't have pre-commit file, it might be removed manually.
+// Bail out if we don't have commit-msg file, it might be removed manually.
 if (!exists(commitMsgFile)) {
-  console.info(`${PACKAGE_NAME_LABEL}: Not found any ${COMMIT_MSG_LABLE} hook, no need to clean the battle field`);
+  console.info(
+    `${PACKAGE_NAME_LABEL}: Not found any ${COMMIT_MSG_LABLE} hook, no need to clean the battle field`
+  );
+
   bailOut();
 }
 
-// If we don't have an old file, we should just remove the pre-commit hook. But
+// If we don't have an old commit-msg file, we should just remove the commit-msg hook.
 if (!exists(`${commitMsgFile}.old`)) {
   fs.unlinkSync(commitMsgFile);
 } else {
-  // if we do have an old precommit file we want to restore that.
+  // But if we do have an old one it must restore. *DON'T BE EVIL*.
   fs.writeFileSync(commitMsgFile, fs.readFileSync(`${commitMsgFile}.old`));
   fs.chmodSync(commitMsgFile, '755');
   fs.unlinkSync(`${commitMsgFile}.old`);
