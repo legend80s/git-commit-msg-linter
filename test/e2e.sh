571cd19354488a651215ca43131d7c246b56231b
legend80s <liuchzong@qq.com>
2023-03-03 01:35:41 +0800

    test: add e2e test

---
+++ test/e2e.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ../commit-msg-linter-test
+
+if [[ -d ../commit-msg-linter ]]; then
+  npm i ../commit-msg-linter --save-dev
+else
+  npm i ../git-commit-msg-linter --save-dev
+fi
+
+echo 1 >> README.md
+
+out=$(git commit -am "test" 2>&1)
+
+# echo out=$out
+
+substr1='Invalid Git Commit Message'
+substr2='Invalid length: Length 4'
+
+if [[ "$out" == *"$substr1"* && "$out" == *"$substr2"* ]]; then
+  echo sucess
+  exit 0
+else
+  echo failed
+  exit 1
+fi
