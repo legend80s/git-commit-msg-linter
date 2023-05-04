b604026bbc6a028db181b32b169f5b8b5250a420
youngseoim <youngseo.im@medistream.co.kr>
2023-05-04 15:10:55 +0900

    fix: isues/38

--- install.js
+++ install.js
@@ -88,7 +88,6 @@ function getTag() {
 
 function genLinterPath() {
   // fix https://github.com/legend80s/git-commit-msg-linter/issues/38
-  // fix https://github.com/legend80s/git-commit-msg-linter/issues/44
   return path.resolve(__dirname, 'commit-msg-linter.js').replace(/\\/g, '/');
 }
 
